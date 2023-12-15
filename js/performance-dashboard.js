d004a22d6424ee93f72ecf28f841fc0776e106a0
Ben Krepp <bkrepp@ctps.org>
2023-12-15 16:22:23 -0500

    Initialized tabs controls for 2 perf areas

--- js/performance-dashboard.js
+++ js/performance-dashboard.js
@@ -2,5 +2,9 @@ function initialize() {
 	// Initialize the accordion control
     $( "#accordion" ).accordion();
 	
+	// Initialize the tabs controls  
+	$( "#ttr-tabs" ).tabs();
+	$( "#cmaq-tabs" ).tabs();
+	
 	return;
 }
