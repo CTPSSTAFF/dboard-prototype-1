574895e641daa969d08e63b0f4633dd0c252645b
Ben Krepp <bkrepp@ctps.org>
2023-12-18 15:55:38 -0500

    Added JS for roadway injuries viz; adjusted 'heightStyle' for accordion control

--- js/performance-dashboard.js
+++ js/performance-dashboard.js
@@ -20,25 +20,56 @@ function initialize_roadway_safety() {
 	  }
 	};
 	
-	var data = [trace1];
+	var fatalityData = [trace1];
 	var layout = {
 	  title: 'Roadway Fatalities in the Boston MPO Region 2011-2019',
 	  barmode: 'stack'
 	};
-	Plotly.newPlot('roadway-fatalities-viz', data, layout);
+	Plotly.newPlot('roadway-fatalities-viz', fatalityData, layout);
+	
+	//////////////////////////////////////
+	// Generate viz of number of injuries
+	var xValue = ['2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019'];
+	var yValue = [15359, 15744, 15823, 16265, 16876, 17339, 17657, 16870, 16104];
+
+	var trace2 = {
+	  x: xValue,
+	  y: yValue,
+	  type: 'bar',
+	  text: yValue.map(String),
+	  textposition: 'auto',
+	  hoverinfo: 'none',
+	  marker: {
+		color: 'rgb(158,202,225)',
+		opacity: 0.6,
+		line: {
+		  color: 'rgb(8,48,107)',
+		  width: 1.5
+		}
+	  }
+	};
+	
+	var injuriesData = [trace2];
+	var layout = {
+	  title: 'Roadway Injuries in the Boston MPO Region 2011-2019',
+	  barmode: 'stack'
+	};
+	Plotly.newPlot('roadway-injuries-viz', injuriesData, layout);
+	
+	
 } // initialize_roadway_safety
 
 function initialize() {
 	// Initialize the accordion control
-    $( "#accordion" ).accordion();
+    $( "#accordion" ).accordion( { heightStyle: "content" } );
 	
 	// Initialize the tabs controls  
-	$( "#roadway-safety-tabs" ).tabs();
-	$( "#transit-safety-tabs" ).tabs();
-	$( "#bridge-pavement-tabs" ).tabs();
-	$( "#tam-tabs" ).tabs();
-	$( "#ttr-tabs" ).tabs();
-	$( "#cmaq-tabs" ).tabs();
+	$( "#roadway-safety-tabs" ).tabs( { heightStyle: "content" } );
+	$( "#transit-safety-tabs" ).tabs( { heightStyle: "auto" } );
+	$( "#bridge-pavement-tabs" ).tabs( { heightStyle: "auto" } );
+	$( "#tam-tabs" ).tabs( { heightStyle: "auto" }  );
+	$( "#ttr-tabs" ).tabs( { heightStyle: "auto" } );
+	$( "#cmaq-tabs" ).tabs( { heightStyle: "auto" } );
 	
 	initialize_roadway_safety();
 	
