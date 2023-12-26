e731ca3dec9b92e9cd9ab2b216791caf1acff3dc
Benjamin Krepp <bkrepp@ctps.org>
2023-12-26 15:04:38 -0500

    Added vizes for interstate pavement good and poor

--- js/performance-dashboard-bootstrap.js
+++ js/performance-dashboard-bootstrap.js
@@ -340,7 +340,6 @@ function roadway_safety_viz() {
 	var yValues_targ = [], yValues_perf = [];
 	
 	// 2 sets of Y values for roadway fatalities
-	
 	var road_fat = _.find(rs_data, function(o) { return o.perf_meas == 'Number of fatalities'; });         
 	yValues_targ = [ road_fat.targ_2023, road_fat.targ_2022, road_fat.targ_2021,
 	                 road_fat.targ_2020, road_fat.targ_2019 ];
@@ -393,9 +392,91 @@ function tam_viz() {
 	
 } // tam_viz
 
+function interstate_pavement_good_viz(xValues, yValues) {
+	var data = [
+	  {
+		x: xValues,
+		y: yValues,
+		type: 'bar',
+		name: 'Target',
+	    text: yValues.map(String),
+	    textposition: 'auto',
+	    hoverinfo: 'none',
+	    opacity: 0.75,
+	  }
+	];
+
+	var layout = {
+		xaxis: { type: 'category' },
+		title: 'Percent of Pavement on the Interstate System in Good Condition'
+	};
+
+	Plotly.newPlot('interstate-pavement-good-viz', data, layout);
+}
+
+
+function interstate_pavement_poor_viz(xValues, yValues) {
+	var data = [
+	  {
+		x: xValues,
+		y: yValues,
+		type: 'bar',
+		name: 'Target',
+	    text: yValues.map(String),
+	    textposition: 'auto',
+		marker: {
+			color: 'orange'
+		},
+	    hoverinfo: 'none',
+	    opacity: 0.75,
+	  }
+	];
+
+	var layout = {
+		xaxis: { type: 'category' },
+		title: 'Percent of Pavement on the Interstate System in Poor Condition'
+	};
+
+	Plotly.newPlot('interstate-pavement-poor-viz', data, layout);	
+}
+
+function noninterstate_pavement_good_viz(xValues, yValues) {
+	
+}
+
+function noninterstate_pavement_poor_viz(xValues, yValues) {
+	
+
+}
 
 function bridge_pavement_viz() {
+	var xValues = [ 'Baseline' , 'Two-year Target (CY 2023)', 'Five-year Target (CY 2025)' ];
+	var yValues = [];
 	
+	// Interstate pavement in GOOD condition
+	var interstate_good = _.find(bp_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in good condition'; });    
+	yValues = [ interstate_good.baseline, interstate_good.two_yr_targ, interstate_good.four_yr_targ ]			 
+	interstate_pavement_good_viz(xValues, yValues);
+	
+	// Interstate pavement in POOR condition
+	var interstate_poor = _.find(bp_data, function(o) { return o.perf_meas == 'Percent of pavements on the Interstate System in poor condition'; });  
+	yValues = [ interstate_poor.baseline, interstate_poor.two_yr_targ, interstate_poor.four_yr_targ ]		 
+	interstate_pavement_poor_viz(xValues, yValues);
+	
+	// Non-interstate NHS pavement in GOOD condition
+	var noninterstate_good = _.find(bp_data, function(o) { return o.perf_meas == 'Percent of pavements on the non-Interstate NHS in good condition'; });  
+	yValues = [ noninterstate_good.baseline, noninterstate_good.two_yr_targ, noninterstate_good.four_yr_targ ]
+	noninterstate_pavement_good_viz(xValues, yValues);	
+	
+	// Non-interstate NHS pavement in POOR condition
+	var noninterstate_poor = _.find(bp_data, function(o) { return o.perf_meas == 'Percent of pavements on the non-Interstate NHS in poor condition'; });  
+	yValues = [ noninterstate_poor.baseline, noninterstate_poor.two_yr_targ, noninterstate_poor.four_yr_targ ]		 
+	noninterstate_pavement_poor_viz(xValues, yValues);	
+	
+	// NHS bridges in GOOD condition
+	
+	
+	// NHS bridges in POOR condition
 
 } // bridge_pavement_viz
 
